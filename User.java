96f1dd3df12d6c99880ed4795e0673d3c6d7f154
Samuel Coker <slc27919@email.vccs.edu>
2016-03-02 15:40:53 -0500

    made javadoc  pretty

--- User.java
+++ User.java
@@ -11,25 +11,29 @@ slc27919@email.vccs.edu
 import java.util.Scanner;
 
 
-/** 
-Write a Java Program to Authenticate a user login for Username and Password.
+/**
+Samuel Coker<br>
+CSC 200<br>
+003N<br>
+27Feb2016<br>
+SLC27919<br>
+slc27919@email.vccs.edu
+<p>
 
-If username and password is correct then display "Welcome Username!" message.
+Write a Java Program to Authenticate a user login for Username and Password.<br>
+
+If username and password is correct then display "Welcome Username!" message.<br>
 
 Else display "Wrong Username or Password!" message.
 
-Requirements:
+<p>
+Requirements:<br>
 
-1. Write comments for Java doc
-
-2. Write Flowchart
-
-3. Write Pseudocode
-
-4. Use VI to write and complie on Terminal
-
-5. Use gitbash to submit your work on github.com
-
+1. Write comments for Java doc<br>
+2. Write Flowchart<br>
+3. Write Pseudocode<br>
+4. Use VI to write and complie on Terminal<br>
+5. Use gitbash to submit your work on github.com<br>
 6. Generate Java Doc */
 
 public class User{
